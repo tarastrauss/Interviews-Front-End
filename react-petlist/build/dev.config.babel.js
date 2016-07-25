9e554be37c763db09f0bbccf1ab34a1cba8825c5
Tara Strauss <tara.m.strauss@gmail.com>
2016-07-25 02:43:10 -0700

    removes react-inline-css and installs css-loader, adds png to webpack config

--- react-petlist/build/dev.config.babel.js
+++ react-petlist/build/dev.config.babel.js
@@ -63,7 +63,9 @@ webpackConfig.plugins = [
 // ------------------------------------
 webpackConfig.module.loaders = [
   { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['babel'] },
-  { test: /\.json$/, loader: 'json' }
+  { test: /\.json$/, loader: 'json' },
+  { test: /\.css$/, loader: "style-loader!css-loader" },
+  { test: /\.png$/, loader: "url-loader?limit=100000" }
 ];
 
 export default webpackConfig;
--- react-petlist/src/index.js
+++ react-petlist/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Test from 'components/Test';
-import InlineCss from "react-inline-css";
+import 'css/index.css';
 
 const dest = document.getElementById('content');
 
@@ -16,57 +16,26 @@ var DisplayBox = React.createClass({
   },
   render: function() {
     return (
-      <InlineCss stylesheet={`
-          & .filterForm {
-            color: #4A4E4E;
-            text-align: center;
-            box-shadow: 5px 5px 5px grey;
-          }
-          & hr {
-            width: 95%;
-            border: 0;
-            height: 1px;
-            background-image: linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.45), rgba(0, 0, 0, 0));
-          }
-          & .filterForm {
-            height: 70px;
-            background-color: #CBD1D1;
-            padding: 2px 0 2px 0;
-          }
-          & .optionLabel {
-            display: inline-block;
-            text-align: left;
-          }
-          & .smallerLabel {
-            font-size: 12px;
-            color: #778786;
-          }
-          & input {
-            margin: 0 8px 0 15px;
-            vertical-align: top;
-          }
-          & .filterDescription {
-            vertical-align: top;
-            margin-right: 20px;
-          }
-      `}>
-        <div className="displayBox">
-          <form className="filterForm" onChange={this.handleServiceRadioBtn}>
-            <hr></hr>
-            <span className="filterDescription"> Looking for: </span>
-            <input type="radio" name="service" value="boarding" />
-            <label className="optionLabel">
-              Boarding <div className="smallerLabel"> at Hosts home </div>
-            </label>
-            <input type="radio" name="service" value="sitting" />
-            <label className="optionLabel">
-              Sitting <div className="smallerLabel"> at my home </div>
-            </label>
-            <hr></hr>
-          </form>
-          <ResultList url={this.state.url} />
+      <div className="displayBox">
+        <div className="headerBox">
+          <div className="logo"></div>
+          <div className="header">Search for a Sitter!</div>
         </div>
-      </InlineCss>
+        <form className="filterForm" onChange={this.handleServiceRadioBtn}>
+          <hr></hr>
+          <span className="filterDescription"> Looking for: </span>
+          <input type="radio" name="service" value="boarding" />
+          <label className="optionLabel">
+            Boarding <div className="smallerLabel"> at Hosts home </div>
+          </label>
+          <input type="radio" name="service" value="sitting" />
+          <label className="optionLabel">
+            Sitting <div className="smallerLabel"> at my home </div>
+          </label>
+          <hr></hr>
+        </form>
+        <ResultList url={this.state.url} />
+      </div>
     );
   }
 });
@@ -140,49 +109,20 @@ var Result = React.createClass({
       var username = this.formatUserName(this.props.data.user.first, this.props.data.user.last);
       var description = this.truncateDescription(this.props.data.description);
       return (
-        <InlineCss stylesheet={`
-          & .result {
-            margin-top: 25px;
-            font-size: 14px;
-          }
-          & .resultTitle {
-            color: #3FBFB8;
-            font-size: 18px;
-            font-weight: bold;
-            margin-bottom: 5px;
-          }
-          & .resultUser {
-            color: #4A4E4E;
-          }
-          & .resultDescription {
-            color: #4A4E4E;
-            margin-top: 5px;
-          }
-          & .resultPetName {
-            color: #4A4E4E;
-            font-style: italic;
-            margin-left: 5px;
-            background-color: #DCDEDE;
-            padding: 2px 5px 2px 5px;
-            border-radius: 5px;
-            box-shadow: 2px 2px 2px grey;
-          }
-        `}>
-          <div className="result">
-            <div className="resultTitle">
-              {this.props.data.title}
-            </div>
-            <span className="resultUser">
-              {username}
-            </span>
-            <span className="resultPetName">
-              {this.props.data.pet.name}
-            </span>
-            <div className="resultDescription">
-              {description}
-            </div>
+        <div className="result">
+          <div className="resultTitle">
+            {this.props.data.title}
           </div>
-        </InlineCss>
+          <span className="resultUser">
+            {username}
+          </span>
+          <span className="resultPetName">
+            {this.props.data.pet.name}
+          </span>
+          <div className="resultDescription">
+            {description}
+          </div>
+        </div>
       );
     }
   });
